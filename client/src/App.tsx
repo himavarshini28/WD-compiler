72f69865880a9d4243744480bbae256481057cfe
himavarshini28 <himavarshinimahapatruni@gmail.com>
2024-12-23 22:25:10 +0530

    Project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit consectetur deleniti possimus et autem ipsum, facere ducimus placeat, officiis ex sit vitae maiores quam, eveniet nesciunt. Consectetur earum odit dolore?
+        <br/>
+        <Button variant="destructive">Test</Button>
+    </>
+  )
+}
+
+export default App

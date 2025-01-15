4d5b3d4685abe26b97ac683a7a0395b2791f087c
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-01-15 18:47:45 +0530

    create Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,8 +2,9 @@
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="text-white w-full h-[calc(100dvh-60px)] flex items-center justify-center flex-col gap-3 pb-9">
+      <h1 className="text-6xl font-bold text-center">WD Compiler</h1>
+      <p className="text-gray-500">Compile HTML, CSS, Javascript code on the go and share it with your friends</p>
     </div>
   )
 }
